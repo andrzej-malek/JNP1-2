31bb810d64bd9864d5adf76754b9f02248f15cb4
Andrzej Małek <56408997+andrzej-malek@users.noreply.github.com>
2019-10-22 14:50:02 +0200

    includes, whitespace update

--- poset.cc
+++ poset.cc
@@ -3,26 +3,25 @@
  * Adam Rozenek i Andrzej Małek
  */
 
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
 
 namespace jnp1
 {
+    namespace sup_func
+    {
+
+    }
 
     using id_graph = unsigned int;
     using graph = unordered_map<id_graph, unorder_set<id_graph>>;
     using dictionary = unordered_map<string, id_graph>;
-
     using id_poset = unsigned long;
 
     unorder_map<id_poset, dictionary> dictionary_map;
     unorder_map<id_poset, id_graph> next_id_graph;
     unorder_map<id_poset, graph> graph_map;
-
-    namespace sup_func
-    {
-
-    }
+    
+    
 }
-
-
-
