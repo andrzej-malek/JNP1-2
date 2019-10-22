3b7f5382e0220ade8ef97dbe018c67c50f8ea782
Adam Rozenek <adam.rozenek@gmail.com>
2019-10-22 14:32:40 +0200

    First commit

--- poset.cc
+++ poset.cc
@@ -1,13 +1,28 @@
-/* Drugie zadanie z JÄ™zykÃ³w i narzÄ™dzi programowania I 19/20
+/* Drugie zadanie z Jêzyków i narzêdzi programowania I 19/20
  * 2019-10-31
- * Adam Rozenek i Andrzej MaÅ‚ek
+ * Adam Rozenek i Andrzej Ma³ek
  */
+
+#include <map>
 
 namespace jnp1
 {
+
+    using id_graph = unsigned int;
+    using graph = unordered_map<id_graph, unorder_set<id_graph>>;
+    using dictionary = unordered_map<string, id_graph>;
+
+    using id_poset = unsigned long;
+
+    unorder_map<id_poset, dictionary> dictionary_map;
+    unorder_map<id_poset, id_graph> next_id_graph;
+    unorder_map<id_poset, graph> graph_map;
 
     namespace sup_func
     {
 
     }
 }
+
+
+
